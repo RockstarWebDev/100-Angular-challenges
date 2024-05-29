531acf827a60bb713423bb4fe8d437b820e0857b
Irshad Khan <webdev727@gmail.com>
2024-05-30 01:08:58 +0530

    accordian with toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { accordianContent } from './accordion/accordion-content.interface';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = '100-Angular-challenges';
+
+  accordianContents: accordianContent[] = [
+    { title: 'section 1', content: 'content of section 1', isOpen: false },
+    { title: 'section 2', content: 'content of section 2', isOpen: false },
+    { title: 'section 3', content: 'content of section 3', isOpen: false },
+  ];
 }
