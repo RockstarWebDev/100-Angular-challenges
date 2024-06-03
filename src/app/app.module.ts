5833486c89100a5441c96e6c28e9527baa88a114
Irshad Khan <webdev727@gmail.com>
2024-06-03 21:37:39 +0530

    Auto focus directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AutoFocusDirective } from './auto-focus.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AutoFocusDirective
   ],
   imports: [
     BrowserModule
